9330d819d1322fc3925f9373c14bc86c1b4c20aa
eduardoMataN <jematanevar@miners.utep.edu>
2022-11-11 12:18:52 -0700

    Designed

--- SimulatedAnnealing.java
+++ SimulatedAnnealing.java
@@ -1,11 +1,46 @@
 public class SimulatedAnnealing {
+    public double increment=0;
+    public double temprature=500;
+    public double coolingRate=0.98;
+    public SchedulingProblem problem;
+    public randomGenerator generator;
+    public Schedule solution;
+    public int scheduledCourses=0;
+    public SimulatedAnnealing(SchedulingProblem problem, Schedule solution){
+        this.problem=problem;
+        this.generator=new randomGenerator()
+        this.solution=solution;
+    }
     public static double probability (double f1, double f2, double temperature){
         if(f1>f2){
             return f1;
         }
         return Math.exp((f1-f2)/temperature);
     }
-    public static int distance(int x1, int y1, int x2, int y2){
-        return ((x2-x1)+(y2-y1));
+
+    public static void coolDown(){
+        this.temperature=this.temperature*this.coolingRate;
     }
+    public Schedule getSolution(){
+        double[4] current=generator.generate()
+        while(current[3]==-1){
+            current=generator.generate()
+        }
+        this.increment=current[3]-this.increment;
+        this.schedule[(int)current[0]][(int)current[1]]=(int)current[2]
+        while(this.temperature>1 && this.scheduledCourses<10){
+            double[4] next=generator.generate();
+            double newIncrement=next[3]-current[3]
+            if(newIncrement>this.increment){
+                current=next;
+                this.schedule[(int)current[0]][(int)current[1]]=(int)current[2]
+                this.generator.schedule=this.schedule;
+                this.increment=newIncrement;
+            }
+            
+        }
+    }
+    
+
+    
 }
--- randomGenerator.java
+++ randomGenerator.java
@@ -1,3 +1,49 @@
+import java.util.*;
+
 public class randomGenerator {
-    public int[] courses;
+    public int courses;
+    public Schedule schedule;
+    public SchedulingProblem problem;
+    public static final double DISTANCE_PENALTY = 2.5d;
+
+    public void randomGenerator(SchedulingProblem problem, Schedule solution){
+        this.problem=problem;
+        this.courses=problem.courses.size()
+        this.schedule=solution;
+    }
+
+    public double[] generate(){
+        //Index 0 is room, Index 1 is timeslot, Index 2 is Course, Index 3 is Value
+        double value=0;
+        double[4] result=[-1,-1,-1,-1];
+        Random random=new Random();
+        int randomClassIndex=((int) (random.nextDouble() * 70)) + 30;
+        int randomRoomIndex=(int) (random.nextDouble() * this.problem.rooms.size());
+        int randomTimeIndex=(int)(random.nextDouble()*10);
+        if((this.schedule[randomRoomIndex][randomTimeIndex]<0) ||(this.schedule[randomRoomIndex][randomTimeIndex]>this.problem.rooms.size())){
+            Room currentRoom=this.problem.rooms.get(randomRoomIndex);
+            Course currentCourse=this.problem.courses.get(randomClassIndex);
+            if(currentCourse.timeSlotValues[randomTimeIndex]!=0){
+                value+=currentCourse.timeSlotValues[randomTimeIndex];
+                value+=currentCourse.value;
+                if(currentRoom.b!=currentCourse.preferredLocation){
+                    Building b1 = currentRoom.b;
+                    Building b2 = currentCourse.preferredLocation;
+                    double xDist = (b1.xCoord - b2.xCoord) * (b1.xCoord - b2.xCoord);
+                    double yDist = (b1.yCoord - b2.yCoord) * (b1.yCoord - b2.yCoord);
+                    double dist = Math.sqrt(xDist + yDist);
+
+                    value -= DISTANCE_PENALTY * dist;
+                }
+                result[0]=new Double(randomRoomIndex);
+                result[1]=new Double(randomTimeIndex);
+                result[2]=new Double(randomClassIndex);
+                result[3]=value;
+                
+                
+                
+            }
+        }
+        return result;
+    }
 }
