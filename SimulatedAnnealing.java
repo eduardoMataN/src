29eff80be09c7825f017083a9e9f80257c3bb250
Tovar <btovar@miners.utep.edu>
2022-11-13 23:11:02 -0700

    retry

--- SimulatedAnnealing.java
+++ SimulatedAnnealing.java
@@ -1,25 +1,29 @@
 import java.util.*;
+
 public class SimulatedAnnealing {
-    public double increment=0;
-    public double temprature=500;
-    public double coolingRate=0.98;
+    public double increment = 0;
+    public double temprature = 500;
+    public double coolingRate = 0.98;
     public SchedulingProblem problem;
     public randomGenerator generator;
     public Schedule solution;
-    public int scheduledCourses=0;
-    public SimulatedAnnealing(SchedulingProblem problem, Schedule solution){
-        this.problem=problem;
-        this.generator=new randomGenerator()
-        this.solution=solution;
-    }
-    public static double probability (double f1, double f2){
-        
-        return Math.exp((f1-f2)/this.temperature);
+    public int scheduledCourses = 0;
+
+    public SimulatedAnnealing(SchedulingProblem problem, Schedule solution) {
+        this.problem = problem;
+        this.generator = new randomGenerator();
+        this.solution = solution;
     }
 
-    public static void coolDown(){
-        this.temperature=this.temperature*this.coolingRate;
+    public static double probability(double f1, double f2) {
+
+        return Math.exp((f1 - f2) / this.temperature);
     }
+
+    public static void coolDown() {
+        this.temperature = this.temperature * this.coolingRate;
+    }
+
     public Schedule getSolution(){
         double[4] current=generator.generate()
         while(current[3]==-1){
@@ -52,7 +56,5 @@ public class SimulatedAnnealing {
         }
         return this.schedule;
     }
-    
 
-    
 }
