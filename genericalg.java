25f2f7326cc5426e32020d5a702a78f9464f345c
Tovar <btovar@miners.utep.edu>
2022-11-13 21:36:50 -0700

    not completed yet

---
+++ genericalg.java
@@ -0,0 +1,18 @@
+//import files from main for 'population'=all info on classes
+
+public class genericalg {
+    int population;
+
+    void println(compute_fitness());
+
+    int compute_fitness(population){
+        return 0;
+    }
+    for(int i=0;i>0;i++){
+        //selection
+        //crossover-----select parents for reproduction
+        //mutation
+        //compute fitness
+        //loop until population had converged then stop
+    }
+}
