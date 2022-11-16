34aeb592d1b670a2af0ba05d71028292790b395c
eduardoMataN <jematanevar@miners.utep.edu>
2022-11-15 17:03:11 -0700

    Added more to genetic

--- individual.java
+++ individual.java
@@ -1,3 +1,14 @@
 public class individual {
-    Schedule solution;
+    int course;
+    int building;
+    int room;
+    double value;
+
+    public individual(){
+
+    }
+
+    public individual(int course, int building, int room, double value){
+        
+    }
 }
--- population.java
+++ population.java
@@ -1,4 +1,19 @@
 import java.util.*;
 public class population {
-    ArrayList <Individual> individuals;
+    int size;
+    ArrayList <individual> individuals;
+
+    public individual getFittest(){
+        double max=0;
+        int pos=0;
+        for(int i=0;i<this.individuals.size();i++){
+            if (individuals.get(i).value>max){
+                max=individuals.get(i).value;
+                pos=i;
+            }
+        }
+        return this.individuals.get(pos);
+    }
 }
+
+
